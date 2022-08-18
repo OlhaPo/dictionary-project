e67bd55e4efa7f294508815e21303e4db51bd3a6
Olya <ponomaryova.olya.y@gmail.com>
2022-08-18 15:21:39 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <header className="App-header"></header>
+      </div>
+      <footer>
+        This project was coded by Olya Po and is{" "}
+        <a href="https://github.com/OlhaPo/dictionary-project" target="_blank">
+          open-sourced on GitHub{" "}
+        </a>
+        and{" "}
+        <a href="https://playful-truffle-fafe7a.netlify.app/" target="_blank">
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
