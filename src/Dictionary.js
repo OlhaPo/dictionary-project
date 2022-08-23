5b58296c98ebec5d28a330a72f04f0e452447132
Olya <ponomaryova.olya.y@gmail.com>
2022-08-23 18:54:36 +0200

    Add styling to the app

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -29,7 +29,7 @@ export default function Dictionary(props) {
     let pexelsApiKey =
       "563492ad6f917000010000019ef7d18463264cf48eda5e29d2fe9ddb";
 
-    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=8`;
     let headers = { Authorization: `Bearer ${pexelsApiKey}` };
 
     axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
